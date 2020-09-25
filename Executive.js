0aff85ef4216f87bf4915357525a2e83a919d507
Kiran Bisarya <kbisarya@ku.edu>
2020-09-25 17:51:52 -0500

    Add GameplayAI Class

--- Executive.js
+++ Executive.js
@@ -38,6 +38,6 @@ class Executive {
 		document.getElementById("controls").style.display = "";
 		document.getElementById("both_boards").style.display = "";
 		document.getElementById("switch-turn").style.display = "none";
-		this.game = new Gameplay(this.rows, this.cols, this.numShips);
+		this.game = new GameplayAI(this.rows, this.cols, this.numShips); //Change to create new InitGame() function and call GameplayAI there
 	}
 }
